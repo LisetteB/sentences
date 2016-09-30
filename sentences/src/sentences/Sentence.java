da38c49d5a11b3c5077a516ca5c22028a1cad523
Lisette Baijens <lisette_bayens@hotmail.com>
2016-09-30 11:29:31 +0200

    Eerste werkende sentence checker in browser

---
+++ sentences/src/sentences/Sentence.java
@@ -0,0 +1,102 @@
+package sentences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sentence {
+	private List<Element> sentence;
+
+	public List<Element> getSentence() {
+		return sentence;
+	}
+	
+	public void addToSentence(Element e){
+		sentence.add(e);
+		System.out.println("from Sentence class: now the complete sentence will be " + sentence);
+	}
+	
+	Sentence(){
+		sentence = new ArrayList<>();
+	}
+	
+	public String toString(){
+		String stringRepresentation = "";
+		for(int i=0; i<sentence.size(); i++){
+			stringRepresentation += sentence.get(i).getWordSequence() + " ";
+		}
+		System.out.println("the toString method is called " + stringRepresentation);
+		return stringRepresentation;
+	}
+	
+	public void clearSentence(){
+		sentence = new ArrayList<>();
+	}
+	
+	/*
+	 * loop through the sentence, 
+	 * if the types of two elements next to each other match
+	 * merge them and insert the resulted combination in the sentence,
+	 * in place of the two elements
+	 * and call the deduction function again
+	 * 
+	 * if there is only one element left in the word_array
+	 * Or if there are no more possible matches
+	 * return the sentence
+	 */
+	public List<Element> deduction(){
+		if(getSentence().size() > 1){
+			Element leftElement;
+			Element rightElement;
+			
+			for(int i=0; i<getSentence().size()-1; i++){
+				leftElement = getSentence().get(i);
+				rightElement = getSentence().get(i+1);
+
+				if(leftElement.getType().needsRightArgument && (leftElement.getType().typeRight.equals(rightElement.getType().typeComplete))){
+					Element combination = mergeRight(leftElement, rightElement);
+					getSentence().set(i,combination);
+					getSentence().remove(i+1);
+					deduction();
+				}
+				if(rightElement.getType().needsLeftArgument && (rightElement.getType().typeLeft.equals(leftElement.getType().typeComplete))){
+					Element combination = mergeLeft(leftElement, rightElement);
+					getSentence().set(i,combination);
+					getSentence().remove(i+1);
+					deduction();
+				}
+			}
+			return getSentence();	
+		}else {
+			return getSentence();	
+		}	
+	}
+	
+	public boolean isSentence(){
+		List<Element> result = deduction();
+		if(result.size() == 1 && sentence.get(0).getType().typeComplete.equals("s")){
+			return true;
+		}
+		else return false;
+	}
+	
+	/*
+	 * mergeRight and mergeLeft return a new Element that's a combination of two other elements
+	 * "np/n -> rechts een n geeft np
+	 * "np\s" -> links een np geeft s
+	 */
+	public Element mergeRight(Element leftElement, Element rightElement){
+		Type type = new Type(leftElement.getType().typeLeft);
+		type.findProperties();
+		String word_sequence = leftElement.getWordSequence() + " " + rightElement.getWordSequence();
+		Element combination = new Element(word_sequence, type);
+		return combination;
+	}
+	
+	public Element mergeLeft(Element leftElement, Element rightElement){
+		Type type = new Type(rightElement.getType().typeRight);
+		type.findProperties();
+		String word_sequence = leftElement.getWordSequence() + " " + rightElement.getWordSequence();
+		Element combination = new Element(word_sequence, type);
+		return combination;
+	}
+}
--- sentences/src/sentences/SentenceChecker.java
+++ sentences/src/sentences/SentenceChecker.java
@@ -3,17 +3,28 @@ package sentences;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SentenceChecker {
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class SentenceChecker implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private List<Element> database;
-	private List<Element> sentence;
+	private Sentence sentence;
+	private String result;
 	
-	public SentenceChecker(){
-		setDatabase();
-		sentence = new ArrayList<>();
-		sentence.add(database.get(1));
-		sentence.add(database.get(0));
-		sentence.add(database.get(3));
+	public String getResult(){
+		return this.result;
+	}
+	
+	public List<Element> getDatabase() {
+		return database;
 	}
 	
 	public void setDatabase(){
@@ -22,63 +33,53 @@ public class SentenceChecker {
 		database.add(new Element("le", new Type("np/n")));
 		database.add(new Element("chien", new Type("n")));
 		database.add(new Element("dort", new Type("np\\s")));
-		database.add(new Element("jean", new Type("np")));		
+		database.add(new Element("jean", new Type("np")));
+		database.add(new Element("mange", new Type("(np\\s)/np")));
 	}
-	
-	public List<Element> deduction(){
-		if(sentence.size() > 1){
-			Element leftElement;
-			Element rightElement;
-			
-			for(int i=0; i<sentence.size()-1; i++){
-				leftElement = sentence.get(i);
-				rightElement = sentence.get(i+1);
 
-				if(leftElement.getType().needsRightArgument && (leftElement.getType().typeRight.equals(rightElement.getType().typeComplete))){
-					Element combination = mergeRight(leftElement, rightElement);
-					sentence.set(i,combination);
-					sentence.remove(i+1);
-					deduction();
-				}
-				if(rightElement.getType().needsLeftArgument && (rightElement.getType().typeLeft.equals(leftElement.getType().typeComplete))){
-					Element combination = mergeLeft(leftElement, rightElement);
-					sentence.set(i,combination);
-					sentence.remove(i+1);
-					deduction();
-				}
-			}
-			return sentence;	
-		}else {
-			return sentence;	
-		}	
+	public Sentence getSentence() {
+		return sentence;
+	}
+
+	private void setSentence(Sentence sentence) {
+		this.sentence = sentence;
 	}
 	
-	/*
-	 * mergeRight and mergeLeft return a new Element that's a combination of two other elements
-	 * "np/n -> rechts een n geeft np
-	 * "np\s" -> links een np geeft s
-	 */
-	public Element mergeRight(Element leftElement, Element rightElement){
-		Type type = new Type(leftElement.getType().typeLeft);
-		type.findProperties();
-		String word_sequence = leftElement.getWordSequence() + " " + rightElement.getWordSequence();
-		Element combination = new Element(word_sequence, type);
-		return combination;
+	public SentenceChecker(){
+		setDatabase();		
+		setSentence(new Sentence());
+//		getSentence().add(database.get(1));
+//		getSentence().add(database.get(0));
+//		getSentence().add(database.get(3));
 	}
 	
-	public Element mergeLeft(Element leftElement, Element rightElement){
-		Type type = new Type(rightElement.getType().typeRight);
-		type.findProperties();
-		String word_sequence = leftElement.getWordSequence() + " " + rightElement.getWordSequence();
-		Element combination = new Element(word_sequence, type);
-		return combination;
+	public String addToSentence(Element e){
+		sentence.addToSentence(e);
+		System.out.println("the complete sentence is now " + sentence);
+		return null;
+	}
+	public String clearSentence(){
+		sentence.clearSentence();
+		result = "";
+		return null;
+	}
+	
+	public String checkSentence(){
+		boolean isSentence = sentence.isSentence();
+		if(isSentence){
+			result = "Bien fait tu as construit une phrase correcte!";
+		}else{
+			result = "Mais non... C'est incomprehensable...";
+		}
+		return null;
 	}
 	
 	public static void main(String[] args){
 		SentenceChecker sc = new SentenceChecker();
-		List<Element> deductionResult = sc.deduction();
-		System.out.println(deductionResult);	
-
-		
+		Sentence s = new Sentence();
+		s.addToSentence(sc.database.get(1));
+		s.addToSentence(sc.database.get(2));
+		s.addToSentence(sc.database.get(3));
+		System.out.println(s.isSentence());			
 	}
 }
